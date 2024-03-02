8fe6048fb6c372689897d6d4a1ac8ff5482b4486
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-02 09:50:11 +0100

    updated

--- src/Components/pages/Dashboard.jsx
+++ src/Components/pages/Dashboard.jsx
@@ -2,30 +2,29 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { API_URL } from "../constants/Api";
 import { useNavigate } from "react-router-dom";
-import Navbar from "./Navbar";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   const [tokenMatch, setTokenMatch] = useState(false);
   const [data, setData] = useState(null); // State to store user data
-  
+  const URL = `${API_URL}/auth/uploadProfilePic`
 
   useEffect(() => {
     const verifyToken = async () => {
       try {
         const token = localStorage.getItem('token')
-        const {data:user} = await axios.get(`${API_URL}/v1/api/getUser`, {
-          headers:{
+        const { data: user } = await axios.get(`${API_URL}/v1/api/getUser`, {
+          headers: {
             "Authorization": `Bearer ${token}`
           }
         });
 
         console.log(user);
-          setLoading(false);
-          setTokenMatch(true);
-          setData(user)
-        
+        setLoading(false);
+        setTokenMatch(true);
+        setData(user)
+
       } catch (error) {
         console.log('Error verifying token:', error);
         console.log(error.response);
@@ -49,16 +48,153 @@ const Dashboard = () => {
     navigate('/login');
     return null;
   }
+  // const changeFile = (e) => {
+  //   let myImage = e.target.files[0];
+  //   let reader = new FileReader();
+  //   reader.readAsDataURL(myImage);
+  //   reader.onload = () => {
+  //     setMyFile(reader.result);
+  //   };
+  // };
+
+  // const uploadFile = () => {
+  //   axios.post(URL, { myFile }).then((res) => {
+  //     setData(res.data);
+  //   });
+  // };
+
 
   return (
     <>
-    {/* <Navbar/> */}
-      <h1 className="text-3xl font-bold text-center mt-4">Dashboard</h1>
-      <div className="mt-4 bg-red-700">
-        <h1 className="text-2xl font-bold">{data.firstName}</h1>
-        <h1 className="text-2xl font-bold">{data.lastName}</h1>
-        <p className="text-lg">{data.email}</p>
-      </div>
+
+      <main className="relative">
+        {/* Glass effect overlay */}
+        <div className="absolute inset-0 bg-gradient-to-b from-[ rgba(84, 87, 88, 0.5)] to-[rgba(105, 108, 108, 0.5)] mix-blend-overlay"></div>
+
+        <nav className="bg-[#1D2021] z-10 sticky top-0 w-full pt-4">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-18">
+              <div className="flex items-center">
+                <div className="flex-shrink-0 flex justify-center mb-10">
+                  <img
+                    className="h-8 w-8 bg-b mt-2"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-bold"
+                  >
+                    Welcome, {data.firstName}
+                  </a>
+                </div>
+
+              </div>
+              <div className="">
+                <div className="ml-4 flex items-center md:ml-6">
+                  <div className="ml-3 relative">
+                    <div>
+                      <button
+                        className="max-w-xs bg-[#861C44] rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                        id="user-menu"
+                        aria-haspopup="true"
+                      >
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          // src={user.profilePicture} 
+                          alt=""
+                        />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+
+        <div className="flex flex-col md:flex-row lg:mx-auto md:justify-center md:items-center md:space-x-4 bg-[#272A2B] p-4">
+          <div className="w-full md:w-1/2">
+            <button
+              className="max-w-xs bg-[#861C44] rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white cursor-pointer mx-auto"
+              id="user-menu"
+              aria-haspopup="true"
+            >
+              <span className="sr-only">Open user menu</span>
+              <img
+                className="h-64 w-64 rounded-full"
+                // src={user.profilePicture} 
+                alt=""
+              />
+            </button>
+            <div className="item-center border border-red-100 flex-col flex justify-center text-gray-300 text-center mt-4">
+              <h1 className="font-sans font-bold text-xl">{data.firstName} {data.lastName}</h1>
+              <p>He/she</p>
+              <p>{data.email}</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center w-full md:w-1/2 mt-4 md:mt-0">
+            <input
+              type="file"
+              // onChange={(e) => changeFile(e)}
+              className="mb-4 w-32 focus:bg-slate-500 rounded outline-none"
+            />
+            <button
+              className="bg-slate-500 p-2 rounded-lg text-white mb-4"
+            // onClick={uploadFile}
+            >
+              Upload File
+            </button>
+            <div className="mb-4">
+              <p className="text-gray-300 font-bold">Bio</p>
+              <input type="text" width={500} height={500} className="bg-white-100 w-96 h-40 text-red-100" />
+            </div>
+            <div>
+              <button className="bg-green-700 text-gray-300 rounded border-none p-2 cursor-pointer">Save</button>
+            </div>
+          </div>
+        </div>
+
+        <footer className="bg-[#272A2B] flex-shrink-0 flex justify-center">
+          <div className="flex">
+            <img
+              className="h-8 w-8 bg-b mt-2"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+              alt="Workflow"
+            />
+            <p className="text-gray-300 ml-2 bg mt-2">© 2024 Authentication, Inc.</p>
+          </div>
+        </footer>
+      </main>
+
+
+
+      {/* <div className="flex flex-col items-center justify-center h-screen">
+        <input
+          type="file"
+          onChange={(e) => changeFile(e)}
+          className="mb-4 w-32 focus:bg-slate-500 rounded outline-none"
+        />
+        <button
+          className="bg-slate-500 p-2 rounded-lg text-white mb-4"
+          onClick={uploadFile}
+        >
+          Upload File
+        </button>
+        {data && (
+          <div className=" text-center">
+            <img
+              src={data.storedImage}
+              className="w-52 h-52 rounded-full"
+              alt=""
+            />
+            <h1 className=" mt-2">Profile Picture</h1>
+          </div>
+
+        )}
+      </div> */}
     </>
   );
 };
--- src/Components/pages/Navbar.jsx
+++ src/Components/pages/Navbar.jsx
@@ -1,87 +1,100 @@
-import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { API_URL } from "../constants/Api";
 
 const Navbar = () => {
-  const [user, setUser] = useState(null);
 
+  const [data, setData] = useState(null); // State to store user data
+  
   useEffect(() => {
-    const fetchUserData = async () => {
-    //   try {
-    //     const response =await axios.post(`${API_URL}/v1/auth/`); // Replace with your actual API endpoint
-    //     setUser(response.data); // Assuming your API response contains user data
-    //   } catch (error) {
-    //     console.error("Error fetching user data:", error);
-    //   }
+    const verifyToken = async () => {
+      try {
+        const token = localStorage.getItem('token')
+        const { data: user } = await axios.get(`${API_URL}/v1/api/getUser`, {
+          headers: {
+            "Authorization": `Bearer ${token}`
+          }
+        });
+
+        console.log(user);
+        setData(user)
+
+      } catch (error) {
+        console.log('Error verifying token:', error);
+        console.log(error.response);
+      }
     };
 
-    fetchUserData();
+    verifyToken();
+
   }, []);
 
   return (
-    <nav className="bg-gray-800 z-10 sticky top-0 w-full">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-              <img
-                className="h-8 w-8 bg-b"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                alt="Workflow"
-              />
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Dashboard
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Team
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Projects
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Calendar
-                </a>
+    <>
+      {/* <nav className="bg-[#1D2021] z-10 sticky top-0 w-full">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-8 w-8 bg-b"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                  alt="Workflow"
+                />
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    {data.firstName}
+                  </a>
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Team
+                  </a>
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Projects
+                  </a>
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Calendar
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="hidden md:block">
-            <div className="ml-4 flex items-center md:ml-6">
-              <div className="ml-3 relative">
-                <div>
-                  <button
-                    className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                    id="user-menu"
-                    aria-haspopup="true"
-                  >
-                    <span className="sr-only">Open user menu</span>
-                    {/* <img
-                      className="h-8 w-8 rounded-full"
-                      src={user.profilePicture} // Assuming you have a profilePicture property in the user object
-                      alt=""
-                    /> */}
-                  </button>
+            <div className="hidden md:block">
+              <div className="ml-4 flex items-center md:ml-6">
+                <div className="ml-3 relative">
+                  <div>
+                    <button
+                      className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                      id="user-menu"
+                      aria-haspopup="true"
+                    >
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        // src={user.profilePicture} // Assuming you have a profilePicture property in the user object
+                        alt="profilePic"
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav> */}
+    </>
   );
 };
 
