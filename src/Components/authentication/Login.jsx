37842b22f135a34d5096fb74677e69199da7b8c4
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-07 11:47:01 +0100

    make a changes in login

--- src/Components/authentication/Login.jsx
+++ src/Components/authentication/Login.jsx
@@ -49,64 +49,68 @@ const Login = () => {
 
 
   return (
-    <div className="max-w-md mx-auto mt-20">
-      <h2 className="text-2xl mb-4 text-center font-bold text-blue-600">Login</h2>
-      <form onSubmit={loginFormik.handleSubmit} className="space-y-4">
-        <div>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-            Email Address
-          </label>
-          <div className="flex items-center border rounded-md px-3 py-2">
-            <AiOutlineUser className="mr-2" />
-            <input
-              name="email"
-              type="text"
-              onChange={loginFormik.handleChange}
-              value={loginFormik.values.email}
-              autoComplete="username"
-              className="mt-1 p-2 flex-grow shadow bg-white  focus:border-blue-500 rounded-lg"
-              placeholder="Email Address"
-            />
-          </div>
-        </div>
+    <>
+      <div className="pt-44">
+        <div className="max-w-md mx-auto shadow-lg bg-[#121212] p-5 rounded text-gray-300">
+          <h2 className="text-2xl mb-4 text-center font-bold text-blue-600 pt-4">Login</h2>
+          <form onSubmit={loginFormik.handleSubmit} className="space-y-4">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-300">
+                Email Address
+              </label>
+              <div className="flex items-center border rounded-md px-3 py-2">
+                <AiOutlineUser className="mr-2" />
+                <input
+                  name="email"
+                  type="text"
+                  onChange={loginFormik.handleChange}
+                  value={loginFormik.values.email}
+                  autoComplete="username"
+                  className="mt-1 p-2 flex-grow shadow bg-white  focus:border-blue-500 rounded-lg"
+                  placeholder="Email Address"
+                />
+              </div>
+            </div>
 
-        <div>
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-            Password
-          </label>
-          <div className="flex items-center border rounded-md px-3 py-2">
-            <AiOutlineLock className="mr-2" />
-            <input
-              name="password"
-              type="password"
-              onChange={loginFormik.handleChange}
-              value={loginFormik.values.password}
-              className="mt-1 p-2 flex-grow shadow bg-white focus:border-blue-500 rounded-lg"
-              placeholder="Password"
-              autoComplete="current-password"
-            />
-          </div>
-        </div>
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium text-gray-300">
+                Password
+              </label>
+              <div className="flex items-center border rounded-md px-3 py-2">
+                <AiOutlineLock className="mr-2" />
+                <input
+                  name="password"
+                  type="password"
+                  onChange={loginFormik.handleChange}
+                  value={loginFormik.values.password}
+                  className="mt-1 p-2 flex-grow shadow bg-white focus:border-blue-500 rounded-lg"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+              </div>
+            </div>
 
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 outline-none focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-gray-50"
-        >
-          Login
-        </button>
-        <div className="flex justify-between">
-          <div>
-            <input type="checkbox" className="mr-2" />
-            <span>Remember me</span>
-          </div>
-          <Link to="/resetEmail" className="text-blue-600">Forgot password?</Link>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 outline-none focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+            >
+              Login
+            </button>
+            <div className="flex justify-between">
+              <div>
+                <input type="checkbox" className="mr-2" />
+                <span>Remember me</span>
+              </div>
+              <Link to="/resetEmail" className="text-blue-600">Forgot password?</Link>
+            </div>
+            <div className="flex">
+              <p>Don't have an account <span>
+                <Link to='/' className=" text-blue-600">Register</Link></span></p>
+            </div>
+          </form>
         </div>
-        <div className="flex">
-          <p>Don't have an account <span>
-            <Link to='/' className=" text-blue-600">Register</Link></span></p>
-        </div>
-      </form>
-    </div>
+      </div>
+    </>
   );
 };
 
