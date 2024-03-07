5d5a39f7809ae14303de53822150ba0415638145
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-07 11:25:22 +0100

    updated

--- src/Components/dashboard/Dashboard.jsx
+++ src/Components/dashboard/Dashboard.jsx
@@ -100,74 +100,47 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="relative bg-[#383C3F] font-serif min-h-screen w-full ">
-        <nav className="bg-[#1D2021] z-10 sticky top-0 w-full pt-4">
-          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div className="flex items-center justify-between h-18">
-              <div className="flex items-center">
-                <div className="flex-shrink-0 flex justify-center mb-10 ">
-                  <Link to={""}>
-                    <img
-                      className="h-8 w-8  mt-2"
-                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                      alt="Workflow"
-                    />
-                  </Link>
-                  <Link
-                    to="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-bold"
-                  >
-                    Welcome, {data.firstName}
-                  </Link>
-                </div>
+        <div className="relative bg-[#383C3F] font-serif min-h-screen w-full">
+      <nav className="bg-[#1D2021] z-10 sticky top-0 w-full pt-4">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="flex flex-shrink-0 justify-between items-center h-18">
+            <div className="flex items-center">
+              <Link to={""} className="flex items-center text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-bold">
+                <img className="h-6 w-6" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+                <span>Welcome, {data.firstName}</span>
+              </Link>
+            </div>
+            <div className="flex items-center">
+            <div className="  mr-4">
+                <img className="h-12 w-12 rounded-full" src={imageSrc} alt="" />
               </div>
-              <div className="flex items-center ">
-                <div className="ml-4 flex items-center md:ml-6">
-                  <div className="mr-2 relative">
-                    <div>
-                      <button
-                        className="max-w-xs bg-[#861C44] rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                        id="user-menu"
-                        aria-haspopup="true"
-                      >
-                        <span className="sr-only">Open user menu</span>
-                        <div>
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src={imageSrc}
-                            alt=""
-                          />
-                        </div>
-                      </button>
-                    </div>
-                  </div>
-                  <div className=" relative">
-                    <div>
-                      <button
-                        className="max-w-xs  rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                        id="settings-button"
-                        aria-haspopup="true"
-                        onClick={toggleDropdown}
-                      >
-                        <span className="sr-only">Open settings menu</span>
-                        <AiOutlineSetting className="h-6 w-6 text-gray-300" />
-                      </button>
-                    </div>
-                    <div className={`dropdown-content shadow-lg bg-[#2e2c2c] rounded-lg mt-2 py-2 w-32 absolute z-10 ${showDropdown ? '' : 'hidden'}`}>
-                      <Link to={"/dashboard/changeEmail"} className=" px-4 py-2 flex text-[#b5bec3] hover:bg-[#474c53]"> <AiOutlineMail className="mt-1 mr-1" /> Emails</Link>
-                      <Link to={"/dashboard/ChangePassword"} className="flex px-4 py-2 text-[#b5bec3] hover:bg-[#474c53]">
+              <div className="ml-2 relative">
+                <button className="text-gray-300 hover:text-white flex mr-4" onClick={toggleDropdown}>
+                  <AiOutlineSetting className="h-6 w-6" /> Settings
+                </button>
+                {showDropdown && (
+                  <div className="absolute z-10 right-0 mt-2 w-48 bg-gray-800 rounded-md shadow-lg">
+                    <div className="py-1">
+                      <Link to="/dashboard/changeEmail" className="block px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white">
+                        <AiOutlineMail className="inline-block mr-2" /> Emails
+                      </Link>
+                      <Link to="/dashboard/ChangePassword" className="block px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white">
                         <BsShieldLock className="inline-block text-gray-300 text-4xl mr-2" />
-                        Change password</Link>
+                        Change password
+                      </Link>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
+              
             </div>
           </div>
-        </nav>
+        </div>
+      </nav>
 
-        <div className="flex flex-col md:flex-row lg:mx-auto md:justify-center md:items-center md:space-x-4 p-4 min-h-screen">
-          <div className="w-full flex md:w-1/2 p-10 shadow-2xl">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row lg:mx-auto md:justify-center md:items-center md:space-x-4 p-4">
+          <div className="w-full flex md:w-1/2 p-8 shadow-2xl">
             <div className="flex flex-col mx-auto space-y-4">
               <h3 className="text-gray-200 text-center">Profile picture</h3>
               <div className="max-w-xs rounded-full flex flex-col items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
@@ -223,28 +196,29 @@ const Dashboard = () => {
             </div>
 
           </div>
-          <div className="flex flex-col items-center justify-center w-full  md:w-1/2 mt-4 md:mt-0 shadow-xl p-5">
+          <div className="flex flex-col items-center justify-center w-full md:w-1/2 mt-4 md:mt-0 shadow-xl p-5">
             <div className="mb-4 md">
               <p className="text-gray-300 font-bold mb-2">Bio</p>
-              <input type="text" width={500} height={500} className="bg-[#626565] w-96 h-40 focus:outline-none rounded-lg text-white font-bold hover:border border-blue-400" />
+              <input type="text" width={500} height={500} className="bg-[#626565] w-full md:w-80 h-40 focus:outline-none rounded-lg text-white font-bold hover:border border-blue-400" />
             </div>
-            <div className="ml-80">
+            <div className="ml-36">
               <button className="bg-green-700 text-gray-300 rounded border-none p-2 cursor-pointer">Save</button>
             </div>
 
           </div>
         </div>
-        <footer className="bg-[#272A2B] flex-shrink-0 flex justify-center text-gray-300 py-2">
-          <div className="flex items-center">
-            <img
-              className="h-8 w-8 bg-b"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-              alt="Workflow"
-            />
-            <p className="ml-2">© 2024 Authentication, Inc.</p>
-          </div>
-        </footer>
       </div>
+      <footer className="bg-[#272A2B] flex-shrink-0 flex justify-center text-gray-300 py-2">
+        <div className="flex items-center">
+          <img
+            className="h-8 w-8 bg-b"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+            alt="Workflow"
+          />
+          <p className="ml-2">© 2024 Authentication, Inc.</p>
+        </div>
+      </footer>
+    </div>
     </>
   );
 };
