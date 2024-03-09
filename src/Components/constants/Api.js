1b4a42931b46171fe9aaed9414647da751d35774
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-09 13:57:20 +0100

    make a changes in Api.js file

--- src/Components/constants/Api.js
+++ src/Components/constants/Api.js
@@ -1,2 +1,9 @@
-export const API_URL='https://learnification.onrender.com'     
-// export const API_URL='http://localhost:8000'
+// export const API_URL='https://learnification.onrender.com'     
+
+
+// export const API_URL= import.meta.env.MODE === "development"? 'http://localhost:8000' : "https://auth-backend-dufd.onrender.com"
+export const API_URL = import.meta.env.PROD? "https://auth-backend-dufd.onrender.com" : "http://localhost:8000"
+// console.log(
+//     import.meta.env.MODE,
+//     import.meta.env.PROD,
+// );
